86366235a16d60c61dc7d11b4e383100cc9fc5c8
Cristian Zuluaga <datacristian1523@gmail.com>
2024-02-12 11:32:24 -0500

    home page scaffolding

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,63 +1,105 @@
 /* eslint-disable react/no-unescaped-entities */
+"use client";
 import React from "react";
+import Main from "./Main";
+import MainFeaturedPost from "./MainFeaturedPost";
+import { Grid } from "@mui/material";
+import FeaturedPost from "./FeaturedPost";
+import Sidebar from "./Sidebar";
+
+const posts = [
+  {
+    title: "Experiments",
+    description:
+      "Explore a variety of experiments and discover new insights. From cutting-edge technologies to scientific breakthroughs, Navika brings the latest experiments to your fingertips.",
+  },
+  {
+    title: "Todo List",
+    description:
+      "Stay organized with Navika's Todo List feature. Plan your day, set priorities, and accomplish tasks efficiently. Never miss a deadline again!",
+  },
+  {
+    title: "Profile Management",
+    description:
+      "Manage your profile seamlessly with Navika. Update your information, change your preferences, and personalize your Navika experience.",
+  },
+];
+
+const mainFeaturedPost = {
+  title: "Title of a longer featured blog post",
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: "https://source.unsplash.com/random?wallpapers",
+  imageText: "main image description",
+  linkText: "Continue reading…",
+};
+
+const featuredPosts = [
+  {
+    title: "Featured post",
+    date: "Nov 12",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random?wallpapers",
+    imageLabel: "Image Text",
+  },
+  {
+    title: "Post title",
+    date: "Nov 11",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random?wallpapers",
+    imageLabel: "Image Text",
+  },
+  {
+    title: "Post title",
+    date: "Nov 11",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random?wallpapers",
+    imageLabel: "Image Text",
+  },
+];
+
+const sidebar = {
+  title: "About",
+  description:
+    "Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.",
+  archives: [
+    { title: "March 2020", url: "#" },
+    { title: "February 2020", url: "#" },
+    { title: "January 2020", url: "#" },
+    { title: "November 1999", url: "#" },
+    { title: "October 1999", url: "#" },
+    { title: "September 1999", url: "#" },
+    { title: "August 1999", url: "#" },
+    { title: "July 1999", url: "#" },
+    { title: "June 1999", url: "#" },
+    { title: "May 1999", url: "#" },
+    { title: "April 1999", url: "#" },
+  ],
+};
 
 const Home = () => {
   return (
     <div className="container mx-auto mt-8 w-full">
-      <h1 className="text-4xl font-bold mb-6">Welcome to Navika</h1>
-
-      <section className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">Experiments</h2>
-        <p>
-          Explore a variety of experiments and discover new insights. From
-          cutting-edge technologies to scientific breakthroughs, Navika brings
-          the latest experiments to your fingertips.
-        </p>
-      </section>
-
-      <section className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">Todo List</h2>
-        <p>
-          Stay organized with Navika's Todo List feature. Plan your day, set
-          priorities, and accomplish tasks efficiently. Never miss a deadline
-          again!
-        </p>
-      </section>
-
-      <section className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">Profile Management</h2>
-        <p>
-          Manage your profile seamlessly with Navika. Update your information,
-          change your preferences, and personalize your Navika experience.
-        </p>
-      </section>
-
-      <section className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">Weather</h2>
-        <p>
-          Stay informed about the weather conditions in your area. Navika
-          provides real-time weather updates to help you plan your activities
-          accordingly.
-        </p>
-      </section>
-
-      <section className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">Notifications</h2>
-        <p>
-          Receive timely notifications about important events, updates, and
-          personalized alerts. Navika keeps you in the loop and ensures you
-          never miss a beat.
-        </p>
-      </section>
-
-      <section>
-        <h2 className="text-2xl font-bold mb-4">Reminders</h2>
-        <p>
-          Set reminders for crucial tasks and events. Navika's reminder feature
-          helps you stay organized and ensures you never forget important
-          deadlines or appointments.
-        </p>
-      </section>
+      <main>
+        <MainFeaturedPost post={mainFeaturedPost} />
+        <Grid container spacing={4}>
+          {featuredPosts.map((post) => (
+            <FeaturedPost key={post.title} post={post} />
+          ))}
+        </Grid>
+        <Grid container spacing={5} sx={{ mt: 3 }}>
+          <Main title="From the firehose" posts={posts} />
+          <Sidebar
+            title={sidebar.title}
+            description={sidebar.description}
+            archives={sidebar.archives}
+          />
+        </Grid>
+      </main>
+      
     </div>
   );
 };
---
+++ src/app/home/Main.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+
+interface MainProps {
+  posts: ReadonlyArray<string>;
+  title: string;
+}
+
+export default function Main(props: any) {
+  const { posts, title } = props;
+
+  return (
+    <Grid
+      item
+      xs={12}
+      md={8}
+      sx={{
+        "& .markdown": {
+          py: 3,
+        },
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        {title}
+      </Typography>
+      <Divider />
+      {posts.map((post: any) => {
+        return (
+          <section className="mb-8" key={post.title}>
+            <h2 className="text-2xl font-bold mb-4">{post.title}</h2>
+            <p>{post.description}</p>
+          </section>
+        );
+      })}
+    </Grid>
+  );
+}
